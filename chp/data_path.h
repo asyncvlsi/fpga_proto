0fa29bc582f2da9b5aba46ed52ff243c8b2539a1
ruslan <rdashkin94@gmail.com>
2023-06-03 10:52:52 -0400

    clean up

---
+++ chp/data_path.h
@@ -0,0 +1,165 @@
+namespace fpga {
+
+class Condition;
+class StateMachine;
+
+class Data {
+public:
+
+  Data();
+  Data(int, int, int, Process *, StateMachine *, Condition *, Condition *, ActId *, Expr *);
+  Data(int, int, int, Process *, StateMachine *, Condition *, Condition *, ActId *, ActId *);
+  ~Data();
+
+  int GetType();
+  Condition *GetCond();
+  Process *GetActScope();
+  StateMachine *GetScope();
+  ActId *GetId();
+
+  void PrintPlain();
+  void PrintVerilog(int, std::string&);
+  void PrintVerilogHSlhs(std::string&);
+  void PrintVerilogHSrhs(std::string&);
+  void PrintVerilogAssignment(std::string &);
+
+private:
+
+  //at which state data processing happens
+  Condition *cond;
+
+  int type; //0 - expression
+            //1 - recv
+            //2 - send
+            //3 - function
+            //4 - unused (only reset to 0)
+
+  int up; //array slice upper boundary
+  int dn; //array slice lower boundary
+
+  ActId *id;  //name of the variable assigning to
+
+  union {
+    //assigning an expression
+    struct {
+      Expr *e; 
+    } assign;
+ 
+    //assigning channel value
+    struct {
+      Condition *up_cond;
+      ActId *chan;
+    } recv;
+
+    //assigning expression to the channel
+    struct {
+      Condition *up_cond;
+      Expr *se;
+    } send;
+
+    //assigning function value
+    struct {
+      mstring_t *name;
+    } func;
+  } u;
+
+  StateMachine *scope;
+  Process *act_scope;
+
+  unsigned int printed:1;
+
+};
+
+class Port {
+public:
+
+  Port();
+  Port(int, int, int, int, ValueIdx *, act_connection *);
+  ~Port();
+
+  int GetDir();
+  act_connection *GetCon();
+  ValueIdx *GetVx();
+  int GetChan();
+  int GetInst();
+
+  void SetInst();
+  void SetCtrlChan();
+
+  void PrintVerilog();
+  void PrintName(std::string &);
+
+private:
+
+  int dir;  //0 - output; 1 - input
+  int width;
+  int ischan; //0 - no, 1 - yes, 2 - control chan(no data)
+  int inst;
+  int reg; //0 - wire, 1 - reg
+
+  ValueIdx *root_id;
+  act_connection *connection;
+
+};
+
+void PrintExpression(Expr *);
+
+class Variable {
+public:
+
+  Variable();
+  Variable(int, int);
+  Variable(int, int, ValueIdx *);
+  Variable(int, int, ValueIdx *, act_connection *);
+  Variable(int, int, int, ValueIdx *, act_connection *);
+  Variable(int, int, int, int, ValueIdx *, act_connection *);
+  
+  void AddDimension(int);
+
+  ValueIdx *GetId();
+  act_connection *GetCon();
+  int GetDimNum();
+  int GetType() { return type; }
+  inline int getDimSize(int n) { return dim[n]; }
+  inline void setDimSize(int n, int ns) { dim[n] = ns; }
+
+  int IsChan();
+  int IsPort();
+
+  inline void MkDyn() { isdyn = 1; }
+
+  void PrintVerilog();
+
+private:
+
+  int type;   //0 - reg, 1 - wire, 2 - inst-inst wire, 3 - internal buffer
+  int ischan; //0 - no, 1 - yes
+  int isport; //0 - no, 1 - yes
+  int isdyn;  //0 - no, 1 - yes
+
+  std::vector<int> dim; //vector of array dimentions width
+
+  ValueIdx *vx;
+  act_connection *id;  //name
+
+};
+
+class Arbiter {
+public:
+
+  Arbiter();
+  ~Arbiter();
+
+  void AddElement(Condition *c);
+
+  void PrintArbiter(FILE *);
+  void PrintInst(int n);
+
+private:
+  
+  std::vector<Condition *> a;
+
+};
+
+
+}
--- chp/state_machine.h
+++ chp/state_machine.h
@@ -2,14 +2,15 @@
 #include <string>
 #include <string.h>
 #include <act/act.h>
+#include "data_path.h"
 
 namespace fpga {
 
 class Data;
 class Port;
 class Variable;
-
 class Arbiter;
+
 class Condition;
 class State;
 class StateMachine;
@@ -215,6 +216,7 @@ public:
   std::map<act_connection*, std::vector<Data *>> GetHSData() { return hs_data; };
   std::vector<Variable *> GetVars();
   inline Variable *GetVarRaw(act_connection *c) { return vm[c]; };
+  inline int GetVarType(act_connection *c) { return vm[c]->GetType(); };
   std::vector<Port *> GetPorts();
   Process *GetProc();
   std::vector<StateMachineInst *> GetInst();
@@ -305,167 +307,4 @@ private:
 
 CHPProject *build_machine (Act *, Process *, int);
 
-/*
- *  Data path class
- */
-
-class Data {
-public:
-
-  Data();
-  Data(int, int, int, Process *, StateMachine *, Condition *, Condition *, ActId *, Expr *);
-  Data(int, int, int, Process *, StateMachine *, Condition *, Condition *, ActId *, ActId *);
-  ~Data();
-
-  int GetType();
-  Condition *GetCond();
-  Process *GetActScope();
-  StateMachine *GetScope();
-  ActId *GetId();
-
-  void PrintPlain();
-  void PrintVerilog(int, std::string&);
-  void PrintVerilogHSlhs(std::string&);
-  void PrintVerilogHSrhs(std::string&);
-  void PrintVerilogAssignment(std::string &);
-
-private:
-
-  //at which state data processing happens
-  Condition *cond;
-
-  int type; //0 - expression
-            //1 - recv
-            //2 - send
-            //3 - function
-            //4 - unused (only reset to 0)
-
-  int up; //array slice upper boundary
-  int dn; //array slice lower boundary
-
-  ActId *id;  //name of the variable assigning to
-
-  union {
-    //assigning an expression
-    struct {
-      Expr *e; 
-    } assign;
- 
-    //assigning channel value
-    struct {
-      Condition *up_cond;
-      ActId *chan;
-    } recv;
-
-    //assigning expression to the channel
-    struct {
-      Condition *up_cond;
-      Expr *se;
-    } send;
-
-    //assigning function value
-    struct {
-      mstring_t *name;
-    } func;
-  } u;
-
-  StateMachine *scope;
-  Process *act_scope;
-
-  unsigned int printed:1;
-
-};
-
-class Port {
-public:
-
-  Port();
-  Port(int, int, int, int, ValueIdx *, act_connection *);
-  ~Port();
-
-  int GetDir();
-  act_connection *GetCon();
-  ValueIdx *GetVx();
-  int GetChan();
-  int GetInst();
-
-  void SetInst();
-  void SetCtrlChan();
-
-  void PrintVerilog();
-  void PrintName(std::string &);
-
-private:
-
-  int dir;  //0 - output; 1 - input
-  int width;
-  int ischan; //0 - no, 1 - yes, 2 - control chan(no data)
-  int inst;
-  int reg; //0 - wire, 1 - reg
-
-  ValueIdx *root_id;
-  act_connection *connection;
-
-};
-
-void PrintExpression(Expr *);
-
-class Variable {
-public:
-
-  Variable();
-  Variable(int, int);
-  Variable(int, int, ValueIdx *);
-  Variable(int, int, ValueIdx *, act_connection *);
-  Variable(int, int, int, ValueIdx *, act_connection *);
-  Variable(int, int, int, int, ValueIdx *, act_connection *);
-  
-  void AddDimension(int);
-
-  ValueIdx *GetId();
-  act_connection *GetCon();
-  int GetDimNum();
-  inline int getDimSize(int n) { return dim[n]; }
-  inline void setDimSize(int n, int ns) { dim[n] = ns; }
-
-  int IsChan();
-  int IsPort();
-
-  inline void MkDyn() { isdyn = 1; }
-
-  void PrintVerilog();
-
-private:
-
-  int type;   //0 - reg, 1 - wire, 2 - inst-inst wire
-  int ischan; //0 - no, 1 - yes
-  int isport; //0 - no, 1 - yes
-  int isdyn;  //0 - no, 1 - yes
-
-  std::vector<int> dim; //vector of array dimentions width
-
-  ValueIdx *vx;
-  act_connection *id;  //name
-
-};
-
-class Arbiter {
-public:
-
-  Arbiter();
-  ~Arbiter();
-
-  void AddElement(Condition *c);
-
-  void PrintArbiter(FILE *);
-  void PrintInst(int n);
-
-private:
-  
-  std::vector<Condition *> a;
-
-};
-
 }
-
-
